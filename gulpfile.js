18d520aa00950b639e8532bd06e8dc21c0c4818e
Jashan Saraon <Jashanp.k99@gmail.com>
2018-12-10 10:07:08 -0600

    'Minify the CSS'

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('css',function () {
     return gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./dist/css'))
 });
 
