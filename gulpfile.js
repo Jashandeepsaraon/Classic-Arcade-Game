5ebbdaa92c1476e9a1280791ca51963eadd21e46
Jashan Saraon <Jashanp.k99@gmail.com>
2018-12-10 09:54:47 -0600

    'Install and autoprefix the CSS file'

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css',function () {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('default', ['css'], function () {
+    gulp.watch('./src/css/**/*.css', ['css']);
+   
+});
