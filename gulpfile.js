807b35c6b772a8ad347f7f7163536814ee8ab2d2
Jashan Saraon <Jashanp.k99@gmail.com>
2018-12-10 10:55:25 -0600

    'Copy over the HTML file'

--- gulpfile.js
+++ gulpfile.js
@@ -14,15 +14,21 @@ gulp.task('css',function () {
         .pipe(gulp.dest('./dist/css'))
 });
 
-gulp.task('default', ['css','images','js'], function () {
+gulp.task('default', ['css','images','js','copy-html'], function () {
     gulp.watch('./src/css/**/*.css', ['css']);
-   
+    gulp.watch('./src/js/**/*.js', ['js']);
+    gulp.watch('./index.html', ['copy-html']);
 });
 
 gulp.task('images', function(){
     return gulp.src('./src/images/**/*.png')
      .pipe(imagemin({verbose: true}))
      .pipe(gulp.dest('./dist/images'))
+});
+
+gulp.task('copy-html', function(){
+    return gulp.src('./index.html')
+     .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('js', function () {
